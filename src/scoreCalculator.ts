a38935758f0118df0659228e71d6d6b5600dbb2d
hwaldmueller <hannah.waldmueller@andrena.de>
2023-03-03 15:38:09 +0100

    rename dices to dice

--- src/scoreCalculator.ts
+++ src/scoreCalculator.ts
@@ -28,26 +28,26 @@ export function calculateScoreForCategory(dice: SetOfDice, category: ScoreCatego
             return 0;
     }
 }
-export function calculateChance(dices: SetOfDice): number {
+export function calculateChance(dice: SetOfDice): number {
     let sum: number = 0;
-    for (let dice of dices) {
-        sum += dice;
+    for (let die of dice) {
+        sum += die;
     }
     return sum;
 }
-export function calculateYahtzee(dices: SetOfDice): number {
-    if (dices.every((die) => die === dices[0])) {
+export function calculateYahtzee(dice: SetOfDice): number {
+    if (dice.every((die) => die === dice[0])) {
         return 50;
     }
     return 0;
 }
 
-export function calculateLargeStrait(dices: SetOfDice): number {
-    let numbersWithoutDuplicates = new Set(dices);
-    if (numbersWithoutDuplicates.size !== dices.length) {
+export function calculateLargeStrait(dice: SetOfDice): number {
+    let numbersWithoutDuplicates = new Set(dice);
+    if (numbersWithoutDuplicates.size !== dice.length) {
         return 0;
     }
-    let sortedDice = dices.sort();
+    let sortedDice = dice.sort();
     if (sortedDice[4]-sortedDice[0] === 4) {
         return 40;
     }
