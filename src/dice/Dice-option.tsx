051a8713151a799c79eaf9adbad561a275e4e96a
hwaldmueller <hannah.waldmueller@andrena.de>
2023-03-14 15:03:03 +0100

    don't visually disable throwDiceButton

--- src/dice/Dice-option.tsx
+++ src/dice/Dice-option.tsx
@@ -5,14 +5,15 @@ import {SetOfDice} from "../app-types";
 
 export function DiceOption(remainingRethrows: number, diceMarkedForRethrow: number[], currentDiceNumbers: [number, number, number, number, number], updateDiceAndRethrowCount: (newRemainingRethrows: number, newDiceNumbers: SetOfDice) => void) {
     function onRollDiceButtonClick() {
-        const newRemainingRethrows = remainingRethrows - 1;
-        const newDiceNumbers = updateDiceNumbers(diceMarkedForRethrow, currentDiceNumbers);
-        updateDiceAndRethrowCount(newRemainingRethrows, newDiceNumbers);
+        if (diceMarkedForRethrow.length > 0) {
+            const newRemainingRethrows = remainingRethrows - 1;
+            const newDiceNumbers = updateDiceNumbers(diceMarkedForRethrow, currentDiceNumbers);
+            updateDiceAndRethrowCount(newRemainingRethrows, newDiceNumbers);
+        }
     }
 
     if (remainingRethrows) {
-        return <button disabled={diceMarkedForRethrow.length === 0}
-                       onClick={() => onRollDiceButtonClick()}
+        return <button onClick={() => onRollDiceButtonClick()}
                        data-testid={TestIds.rollDiceBtn}>Roll selected dice</button>;
     }
     return <div data-testid={TestIds.noRethrowsMsg}>No rethrows remain. Please select a category from the score
