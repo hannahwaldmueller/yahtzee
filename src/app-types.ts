91bc3f72dd0e1fe99cd7e428d34c7145d78d4939
hwaldmueller <hannah.waldmueller@andrena.de>
2023-04-07 20:51:35 +0200

    Updates

--- src/app-types.ts
+++
@@ -1,5 +0,0 @@
-import {ScoreCategories} from "./score/score-categories";
-
-export type ScoreMap = Map<ScoreCategories, number | null>
-
-export type SetOfDice = [number, number, number, number, number]
--- src/test-constants.ts
+++
@@ -1,8 +0,0 @@
-export namespace TestIds {
-    export const confirmButton: string = "confirmButton";
-    export const categorySetMsg: string = "categoryAlreadySetMessage";
-    export const categoryButtonPrefix: string = "categoryButton_";
-    export const rollDiceBtn: string = "rollDiceButton";
-    export const diceButton_1: string = "diceButton_1";
-    export const noRethrowsMsg: string = "noRethrowsMessage";
-}
